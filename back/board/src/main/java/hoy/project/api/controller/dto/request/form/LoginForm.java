95206937cc454dee8608e23b092ab459886f75e7
moonyoungho <ansdudgh98@naver.com>
2023-04-03 02:29:56 +0900

    refactor : "@NotBlank -> @NotEmpty로 변경"

--- back/board/src/main/java/hoy/project/api/controller/dto/request/form/LoginForm.java
+++ back/board/src/main/java/hoy/project/api/controller/dto/request/form/LoginForm.java
@@ -3,18 +3,16 @@ package hoy.project.api.controller.dto.request.form;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 public class LoginForm {
 
-    @NotBlank(message = "아이디를 입력해주세요.")
+    @NotEmpty(message = "아이디를 입력해주세요.")
     private String loginId;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String loginPassword;
-
-    public LoginForm() {
-    }
 
     public LoginForm(String loginId, String loginPassword) {
         this.loginId = loginId;
