7e8a015155a20070d95348317c6aa1812536f1e9
moonyoungho <ansdudgh98@naver.com>
2023-04-03 02:32:44 +0900

    refactor : "인터셉터 url 패턴 추가"

--- back/board/src/main/java/hoy/project/config/WebConfig.java
+++ back/board/src/main/java/hoy/project/config/WebConfig.java
@@ -1,13 +1,21 @@
 package hoy.project.config;
 
+import hoy.project.api.controller.argumentresolver.AccountArgumentResolver;
 import hoy.project.api.interceptor.LogInterceptor;
 import hoy.project.api.interceptor.LoginCheckInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AccountArgumentResolver accountArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -15,6 +23,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/api/account/login","/api/account/signup");
+                .excludePathPatterns("/api/account/login","/api/account/signup","/h2-console");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+        resolvers.add(accountArgumentResolver);
     }
 }
