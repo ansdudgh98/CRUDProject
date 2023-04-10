a4ad1f49022848c36febfdb159c301ae34caa04b
moonyoungho <ansdudgh98@naver.com>
2023-04-10 20:35:43 +0900

    chore : "kill java app remove"

---
+++ back/board/build/generated/'querydsl'/hoy/project/domain/QReply.java
@@ -0,0 +1,67 @@
+package hoy.project.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReply is a Querydsl query type for Reply
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReply extends EntityPathBase<Reply> {
+
+    private static final long serialVersionUID = 1355186673L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReply reply = new QReply("reply");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    public final QAccount account;
+
+    //inherited
+    public final NumberPath<Integer> active = _super.active;
+
+    public final QComment comment;
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedDate = _super.lastModifiedDate;
+
+    public QReply(String variable) {
+        this(Reply.class, forVariable(variable), INITS);
+    }
+
+    public QReply(Path<? extends Reply> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReply(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReply(PathMetadata metadata, PathInits inits) {
+        this(Reply.class, metadata, inits);
+    }
+
+    public QReply(Class<? extends Reply> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.account = inits.isInitialized("account") ? new QAccount(forProperty("account")) : null;
+        this.comment = inits.isInitialized("comment") ? new QComment(forProperty("comment"), inits.get("comment")) : null;
+    }
+
+}
+
