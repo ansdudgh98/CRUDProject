15ee81a77c1e5726fca82fc65a254ae171f814c2
YoungHo Moon <ansdudgh98@naver.com>
2023-04-15 00:48:40 +0900

    refactor : "@NotEmpty -> @NotBlank 삭제"

--- back/board/src/main/java/hoy/project/api/controller/dto/request/form/CommentForm.java
+++ back/board/src/main/java/hoy/project/api/controller/dto/request/form/CommentForm.java
@@ -2,13 +2,14 @@ package hoy.project.api.controller.dto.request.form;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Getter
 public class CommentForm {
 
-    @NotEmpty(message = "제목을 입력하지 않았습니다.")
+    @NotBlank(message = "제목을 입력하지 않았습니다.")
     @Size(max = 100)
     private String content;
 
