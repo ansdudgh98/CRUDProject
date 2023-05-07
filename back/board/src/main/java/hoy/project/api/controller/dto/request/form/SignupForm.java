affb13c2dc3a68e21fe488c00b1fb15f0643691b
ansdudgh98 <ansdudgh98@naver.com>
2023-05-07 22:32:53 +0900

    refactor : "Form Validation @NotEmpty -> @NotBlank로 변경 및 valdation관련 ExceptionController 변경"

--- back/board/src/main/java/hoy/project/api/controller/dto/request/form/SignupForm.java
+++ back/board/src/main/java/hoy/project/api/controller/dto/request/form/SignupForm.java
@@ -14,10 +14,16 @@ public class SignupForm {
     private String signupId;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
-//    @Pattern(regexp = "^[a-zA-Z0-9`~!@#$%^&*()-_=+]+[a-zA-Z0-9`~!@#$%^&*()-_=+]{8,24}$", message = "올바른 비밀번호 형식을 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9`~!@#$%^&*()-_=+]+[a-zA-Z0-9`~!@#$%^&*()-_=+]{8,16}$", message = "올바른 비밀번호 형식을 입력해주세요.")
     private String signupPw;
 
     @NotBlank(message = "이메일 주소를 확인해주세요..")
     @Pattern(regexp = "^[A-Za-z0-9]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    public SignupForm(String signupId, String signupPw, String email) {
+        this.signupId = signupId;
+        this.signupPw = signupPw;
+        this.email = email;
+    }
 }
