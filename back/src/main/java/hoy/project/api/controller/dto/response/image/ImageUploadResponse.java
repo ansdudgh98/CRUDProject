f5785087f9b06f7440d66e199dc492b1f6ce25ce
moonyoungho <ansdudgh98@naver.com>
2023-05-21 11:17:23 +0900

    feat : "ImageUploadResponse 작성"

---
+++ back/src/main/java/hoy/project/api/controller/dto/response/image/ImageUploadResponse.java
@@ -0,0 +1,12 @@
+package hoy.project.api.controller.dto.response.image;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ImageUploadResponse {
+
+    private String url;
+
+}
