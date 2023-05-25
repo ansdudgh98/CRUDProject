82e8e44e4ca47abdc27bf364498ff9c29099eb80
moonyoungho <ansdudgh98@naver.com>
2023-05-26 02:39:42 +0900

    refactor : "repository @Param 파라미터에 추가"

--- back/src/main/java/hoy/project/repository/ReplyRepository.java
+++ back/src/main/java/hoy/project/repository/ReplyRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -14,11 +15,11 @@ public interface ReplyRepository extends JpaRepository<Reply,Long> {
     @Query(value = "select re from Reply re " +
             "join fetch re.account " +
             "where re.id = :id")
-    Reply findReplyById(Long id);
+    Reply findReplyById(@Param("id") Long id);
 
     @Query(value = "select re from Reply re " +
             "join fetch re.account " +
             "where re.comment.id = :commentId and re.active = 1")
-    Slice<Reply> findSliceReplies(Long commentId, Pageable pageable);
+    Slice<Reply> findSliceReplies(@Param("commentId") Long commentId, Pageable pageable);
 
 }
